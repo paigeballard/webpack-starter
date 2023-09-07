f09a7a69033616b0efc3a91d050c97444597180b
Paige Ballard <p.ballardhou@gmail.com>
2023-09-07 09:46:51 -0500

    added devserver plugin, babel and css extract plugin

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import message from './message'
 import './css/style.css'
 
 console.log(message)
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 module.exports = {
     mode: 'development',
@@ -8,13 +9,33 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        compress: true,
+        historyApiFallback: true,
+    },
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -22,5 +43,6 @@ module.exports = {
             filename: 'index.html',
             template: './src/index.html'
         }),
+        new MiniCssExtractPlugin()
     ],
 }
